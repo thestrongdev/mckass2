973154c36ee061214e9d144fc551e0733002933d
Candace <goodsoncj@gmail.com>
2020-11-14 12:37:11 -0500

    assessment 2 practice

---
+++ Challenge.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace mckass2
+{
+    class Challenge
+    {
+        
+        public static double AverageDroids(List<int> droids)
+        {
+            List<int> evenDroids = new List<int>();
+
+            foreach (int droid in droids)
+            {
+                if (droid%2 == 0)
+                {
+                    evenDroids.Add(droid);
+                }
+            }
+
+            double averageEvenDroids = evenDroids.Average();
+
+            return averageEvenDroids;
+        }
+        
+        public static List<string> ConvertPlanets(string[] planets)
+        {
+            List<string> newPlanets = new List<string>();
+
+            for (int i = planets.Length - 1; i >= 0; i--)
+            {
+                newPlanets.Add(planets[i]);
+
+            }
+            return newPlanets;
+        }
+        
+        public static int AddStarWarsCharacters(string[] characters)
+        {
+            int yoda = 0;
+
+            for(int i=0; i<characters.Length; i++)
+            {
+                if(characters[i].Equals("Yoda", StringComparison.OrdinalIgnoreCase))
+                {
+                    yoda = i;
+                }
+            }
+
+            return yoda;
+        }
+
+        public static string DeathStarCombat(Dictionary<string, int> charAttack) //don't feel I've really grasped how to most efficiently handle dictionaries
+        {
+            var values = charAttack.Values;
+            List<int> onlyAttack = new List<int>();
+            string bestAttack;
+            int bestAttackNum;
+
+            foreach (var value in values) //find max value
+            {
+                onlyAttack.Add(value);
+            }
+
+            bestAttackNum = onlyAttack.Max();
+
+            foreach(KeyValuePair<string, int> pair in charAttack)
+            {
+                if(EqualityComparer<int>.Default.Equals(pair.Value, bestAttackNum))
+                {
+                    bestAttack = pair.Key;
+                    return bestAttack;
+                }
+            }
+
+            return "error or nothing in list";
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mckass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TEST AVERAGEDROIDS
+            Console.WriteLine("AVERAGEDROID TEST");
+            List<int> droids = new List<int> { 1, 1, 3, 5, 6, 7, 8, 10, 12, 13, 2, 4}; //evens: 6, 8, 10, 12, 2, 4
+            Console.WriteLine(Challenge.AverageDroids(droids)); //SHOULD OUTPUT 7
+            Console.WriteLine();
+
+            //TEST CONVERTPLANETS
+            Console.WriteLine("CONVERTPLANETS TEST");
+            string[] planets = { "Uranus", "Jupiter", "Saturn", "Pluto?", "Venus", "Earth", "Mars", "Mercury", "Neputune" };
+            List<string> testPlanets = Challenge.ConvertPlanets(planets); //SHOULD REVERSE ^^ ORDER
+
+            foreach (string planet in testPlanets)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.WriteLine();
+
+            //TEST DEATHSTARCOMBAT
+            Console.WriteLine("DEATHSTARCOMBAT TEST");
+            Dictionary<string, int> deathStarStats = new Dictionary<string, int>
+            {
+                {"Obi", 3 },
+                {"Luke", 5},
+                {"Darth", 11},
+                {"Leia", 7},
+                {"Chewbacca", 8},
+                {"C3PO", 45},
+                {"R2D2", 14},
+                {"Han", 10}
+            };
+
+            string theBest = Challenge.DeathStarCombat(deathStarStats);
+            Console.WriteLine(theBest); //SHOULD BE C3PO
+            Console.WriteLine();
+
+            //TEST ADDSTARWARSCHAR
+            Console.WriteLine("ADDSTARWARSCHAR TEST");
+            string [] starChars = {"Obi", "Luke", "Chewbacca", "Yoda", "Han", "Leia", "R2D2", "C3PO", "Darth"};
+            //YODA AT INDEX 3
+            Console.WriteLine(Challenge.AddStarWarsCharacters(starChars));
+
+        }
+    }
+}
